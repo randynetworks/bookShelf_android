1bdeeb7b6627af68e20de0b5a20548960d97c5c0
randynetworks <randyramadhan1973@gmail.com>
2021-10-05 17:34:13 +0700

    feat: SplashScreen & list buku

--- app/src/main/java/com/rr/bookshelf/SplashScreen.java
+++ app/src/main/java/com/rr/bookshelf/SplashScreen.java
@@ -2,13 +2,31 @@ package com.rr.bookshelf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        int delayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, delayed * 3000);
     }
 }
