612b6be8b2f6ee8955570d2179fff7438eb523a5
randynetworks <randyramadhan1973@gmail.com>
2021-10-07 11:25:48 +0700

    feat: fix design card & action btn

--- app/src/main/java/com/rr/bookshelf/BookAdapter.java
+++ app/src/main/java/com/rr/bookshelf/BookAdapter.java
@@ -3,10 +3,13 @@ package com.rr.bookshelf;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -49,6 +52,39 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.bookHolder> {
         } else {
             holder.doneOrUnDone.setText("Udah Beres");
         }
+
+        holder.card_item.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                holder.card_item.setBackgroundColor(Color.rgb(220, 53, 69));
+                holder.title.setTextColor(Color.rgb(255,255,255));
+                holder.author.setTextColor(Color.rgb(255,255,255));
+                holder.publisher.setTextColor(Color.rgb(255,255,255));
+                holder.year.setTextColor(Color.rgb(255,255,255));
+
+
+                holder.doneOrUnDone.setVisibility(View.GONE);
+                holder.btnEdit.setVisibility(View.GONE);
+                holder.btnDelete.setVisibility(View.VISIBLE);
+                holder.text_info.setVisibility(View.VISIBLE);
+                return true;
+            }
+        });
+
+        holder.card_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.card_item.setBackgroundColor(Color.rgb(255, 204, 0));
+                holder.title.setTextColor(Color.rgb(15,52,146));
+                holder.author.setTextColor(Color.rgb(15,52,146));
+                holder.publisher.setTextColor(Color.rgb(15,52,146));
+                holder.year.setTextColor(Color.rgb(15,52,146));
+                holder.doneOrUnDone.setVisibility(View.VISIBLE);
+                holder.btnEdit.setVisibility(View.VISIBLE);
+                holder.btnDelete.setVisibility(View.GONE);
+                holder.text_info.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
@@ -57,8 +93,9 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.bookHolder> {
     }
 
     public static class bookHolder extends RecyclerView.ViewHolder {
-        TextView title, author, publisher, year;
-        Button doneOrUnDone;
+        TextView title, author, publisher, year, text_info;
+        Button doneOrUnDone, btnEdit, btnDelete;
+        LinearLayout card_item;
         public bookHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.textTitle);
@@ -66,6 +103,10 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.bookHolder> {
             publisher = itemView.findViewById(R.id.textPublisher);
             year = itemView.findViewById(R.id.textYear);
             doneOrUnDone = itemView.findViewById(R.id.doneOrUnDone);
+            btnEdit = itemView.findViewById(R.id.btnEdit);
+            btnDelete = itemView.findViewById(R.id.btnDelete);
+            card_item = itemView.findViewById(R.id.card_item);
+            text_info = itemView.findViewById(R.id.text_info);
         }
     }
 }
